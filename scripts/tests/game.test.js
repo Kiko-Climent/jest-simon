49105fd5f957802466c1f48eb372d1867ebacdc2
Kiko Climent <climent.kiko@gmail.com>
2023-11-11 17:58:31 +0000

    Using game state

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore, addTurn, lightsOn } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
 
 
 beforeAll(() => {
@@ -29,6 +29,10 @@ describe("game object contains correct keys", () => {
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
+    });
+    
 });
 
 describe ("newGame works correctly", () => {
